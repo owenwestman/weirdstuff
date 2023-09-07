7a40a3d65585b07a56a2b107e3611e3c755db2fc
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-09-07 14:48:32 -0400

    Update bookmarks.js
    added background color changer

--- bookmarks.js
+++ bookmarks.js
@@ -9,3 +9,14 @@ console.log(images);
 for (var i = 0; i < images.length; i++) {
   images[i].src=" put link here ";
 }
+
+// background color changer
+const accColors = ["black","white","gray","red","blue","green","yellow","orange","purple","pink","brown","cyan"];
+var userColor = prompt("Which color would you like to change the background to?");
+if(accColors.includes(userColor)) {
+    console.log("bccSuccess " + userColor);
+    document.body.style.background = userColor;
+} else if(!accColors.includes(userColor)) {
+    console.log("bccFailure " + userColor);
+    alert("Please use one of the allowed colors in lowercase. Valid colors include: " + accColors);
+}
