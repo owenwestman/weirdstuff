7fa56dcc1601188c2a9e7dbaae0988488a8f5e1c
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-09-18 08:54:22 -0400

    cool update
    added new mystery color

--- bookmarks.js
+++ bookmarks.js
@@ -12,9 +12,11 @@ for (var i = 0; i < images.length; i++) {
 }
 
 // background color changer
-const accColors = ["black","white","gray","red","blue","green","yellow","orange","purple","pink","brown","cyan"];
+const accColors = ["black","white","gray","red","blue","green","yellow","orange","purple","pink","brown","cyan", "aven"];
 var userColor = prompt("Which color would you like to change the background to?");
-if(accColors.includes(userColor)) {
+if (userColor = "aven") {
+  alert("you have chosen the mysterious aven color");
+} else if(accColors.includes(userColor)) {
     console.log("bccSuccess " + userColor);
     document.body.style.background = userColor;
 } else if(!accColors.includes(userColor)) {
