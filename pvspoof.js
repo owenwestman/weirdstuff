c68e27573b013a49740f91076390b1786b4e1cfb
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-02 09:07:47 -0400

    Create pvspoof.js

---
+++ pvspoof.js
@@ -0,0 +1,5 @@
+var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
+console.log(gnum);
+for (i = 0; i < gnum.length; i++) {
+gnum[i].innerHtml = "100";
+}
