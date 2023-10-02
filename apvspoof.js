8c52df7d3b38a20ee51ba246ba78ce120eb993bf
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-02 11:47:47 -0400

    Update apvspoof.js
    almost done

--- apvspoof.js
+++ apvspoof.js
@@ -1,5 +1,12 @@
+javascript:(()=>{
 var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
-console.log(gnum);
-for (i = 0; i < gnum.length; i++) {
-gnum[i].innerHtml = "100";
-}
+var gw = document.getElementsByClassName('v-horizontallayout v-layout v-horizontal v-widget FakeBarGraph v-horizontallayout-FakeBarGraph v-has-width v-has-height');
+/* for (var i = 0; i < gnum.length; i++) {
+gnum[i].innerHTML = '100';
+console.log(gnum[i] + " replaced with 100");
+} */
+for(var i=0; i < gw.length; i++) {
+gw[i].style.width = '100%';
+console.log(gw[i] + " replaced with 100%");
+} 
+})()
