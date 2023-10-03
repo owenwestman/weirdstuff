1e447a8a7d0d9558be36ab5450714cf599cdf520
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-03 10:56:58 -0400

    Finish apvspoof.js
    works good now

--- apvspoof.js
+++ apvspoof.js
@@ -1,19 +1,10 @@
+//paste as bookmark
+
 javascript:(()=>{
-let gval = prompt("Give grades seperated by commas. Set as nan for none.");
+let gval = prompt("Give grades seperated by commas. Set as blank for none.");
 var gar = gval.split(",");
 var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
 var gw = document.getElementsByClassName('v-horizontallayout v-layout v-horizontal v-widget FakeBarGraph v-horizontallayout-FakeBarGraph v-has-width v-has-height');
-/* function rnum(min, max) {
-  let funcans = Math.floor(Math.random() * (min + max));
-  console.log(funcans + ' ' + min + ' ' + max);
-  return funcans;
-}
-*/
-for (var i = 0; i < gar.length; i++) {
-if (gar[i] === 'nan') {
-gar[i] = '';
-}
-}
 for (var i = 0; i < gnum.length; i++) {
 gnum[i].innerHTML = gar[i];
   console.log(gar[i]);
