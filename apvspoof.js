2d6341076ec41efc0cb5ea58e51276c117ada767
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-03 11:07:10 -0400

    Update apvspoof.js
    Added variable width

--- apvspoof.js
+++ apvspoof.js
@@ -2,7 +2,9 @@
 
 javascript:(()=>{
 let gval = prompt("Give grades seperated by commas. Set as blank for none.");
+let wval = prompt("Enter all non-blank grades in order, seperated by commas. Used grades: " + gval);
 var gar = gval.split(",");
+var war = wval.split(",");
 var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
 var gw = document.getElementsByClassName('v-horizontallayout v-layout v-horizontal v-widget FakeBarGraph v-horizontallayout-FakeBarGraph v-has-width v-has-height');
 for (var i = 0; i < gnum.length; i++) {
@@ -10,6 +12,7 @@ gnum[i].innerHTML = gar[i];
   console.log(gar[i]);
 }
 for(var i=0; i < gw.length; i++) {
-gw[i].style.width = '100%';
+gw[i].style.width = parseInt(war[i]) + '%';
+console.log(parseInt(war[i]));
 } 
 })()
