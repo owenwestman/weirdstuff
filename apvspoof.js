a50d58e93a9f53f73ed5804b200ca23621cb2ca5
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-03 08:39:42 -0400

    Update apvspoof.js
    almost done probably

--- apvspoof.js
+++ apvspoof.js
@@ -1,20 +1,22 @@
 javascript:(()=>{
-let mval = prompt("Minimum grade? (1-100)");
-let aval = prompt("Maximum grade? (1-100)");
-parseInt(mval);
-parseInt(aval);
-if (aval === 100) {
-aval = 101;
-}
+let gval = prompt("Give grades seperated by commas. Set as nan for none.");
+var gar = gval.split(",");
 var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
 var gw = document.getElementsByClassName('v-horizontallayout v-layout v-horizontal v-widget FakeBarGraph v-horizontallayout-FakeBarGraph v-has-width v-has-height');
-function rnum(min, max) {
+/* function rnum(min, max) {
   let funcans = Math.floor(Math.random() * (min + max));
   console.log(funcans + ' ' + min + ' ' + max);
   return funcans;
 }
+*/
+for (var i = 0; i < gar.length; i++) {
+if (gar[i] === 'nan') {
+gar[i] = '';
+}
+}
 for (var i = 0; i < gnum.length; i++) {
-gnum[i].innerHTML = rnum(mval,aval).toString();
+gnum[i].innerHTML = gar[i];
+  console.log(gar[i]);
 }
 for(var i=0; i < gw.length; i++) {
 gw[i].style.width = '100%';
