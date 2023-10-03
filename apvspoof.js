d0942c69d10b6cc9afb9e8b78d965f66ed1e1f1d
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-03 08:29:46 -0400

    Update apvspoof.js
    dev save

--- apvspoof.js
+++ apvspoof.js
@@ -1,15 +1,22 @@
 javascript:(()=>{
+let mval = prompt("Minimum grade? (1-100)");
+let aval = prompt("Maximum grade? (1-100)");
+parseInt(mval);
+parseInt(aval);
+if (aval === 100) {
+aval = 101;
+}
 var gnum = document.getElementsByClassName('v-label v-widget InFakeBar v-label-InFakeBar v-label-undef-w');
 var gw = document.getElementsByClassName('v-horizontallayout v-layout v-horizontal v-widget FakeBarGraph v-horizontallayout-FakeBarGraph v-has-width v-has-height');
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
+function rnum(min, max) {
+  let funcans = Math.floor(Math.random() * (min + max));
+  console.log(funcans + ' ' + min + ' ' + max);
+  return funcans;
 }
 for (var i = 0; i < gnum.length; i++) {
-gnum[i].innerHTML = getRndInteger.toString();
-console.log(gnum[i] + " replaced with 100");
+gnum[i].innerHTML = rnum(mval,aval).toString();
 }
 for(var i=0; i < gw.length; i++) {
 gw[i].style.width = '100%';
-console.log(gw[i] + " replaced with 100%");
 } 
 })()
