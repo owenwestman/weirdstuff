c1e5517417ff162b2e7c7ac32983e1f847a20a77
owenwestman <144341005+owenwestman@users.noreply.github.com>
2023-10-19 08:37:44 -0400

    Create drowning.js
    timing bad dw will fix probably

---
+++ drowning.js
@@ -0,0 +1,12 @@
+javascript:(()=>{
+var drw = document.createElement('img');
+drw.src = 'https://static.vecteezy.com/system/resources/thumbnails/027/292/448/small/water-splashes-and-drops-isolated-on-transparent-background-abstract-background-with-blue-water-wave-png.png';
+drw.style.cssText = 'position: fixed; z-index: 9999; width: 100vw; height: 100vh';
+drw.style.left = '0px';
+drw.style.top = '0px';
+document.body.appendChild(drw);
+function shut() {
+window.location.replace('https://owenwestman.neocities.org/drown');
+}
+setTimeout(shut(), 5000);
+})()
